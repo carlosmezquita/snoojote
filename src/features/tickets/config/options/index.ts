97e34f62374286f8980fa1ba64f594f4531b0a3c
Carlos Mezquita <mezquitalobato@gmail.com>
2026-05-16 12:35:51 +0000

    Disable minecraft ticket option

--- src/features/tickets/config/options/index.ts
+++ src/features/tickets/config/options/index.ts
@@ -1,14 +1,12 @@
 import { rplaceTicket } from './rplace.js';
 import { generalTicket } from './general.js';
 import { spamTicket } from './spam.js';
-import { minecraftTicket } from './minecraft.js';
 import { type TicketOptionConfig } from '../TicketConfig.js';
 
 export const ticketOptions: Record<string, TicketOptionConfig> = {
     [generalTicket.id]: generalTicket,
     [spamTicket.id]: spamTicket,
     [rplaceTicket.id]: rplaceTicket,
-    [minecraftTicket.id]: minecraftTicket,
 };
 
-export const ticketOptionsList = [generalTicket, spamTicket, rplaceTicket, minecraftTicket];
+export const ticketOptionsList = [generalTicket, spamTicket, rplaceTicket];
